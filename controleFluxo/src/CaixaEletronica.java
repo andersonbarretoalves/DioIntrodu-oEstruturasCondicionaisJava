a04210c4c36a32066c9f874dafdc6e0562ea76eb
andersonbarretoalves <andersonguitar2@gmail.com>
2023-03-27 19:06:26 -0300

    Segundo arquivo Criado  ResultadoEscolar

--- controleFluxo/src/CaixaEletronica.java
+++ controleFluxo/src/CaixaEletronica.java
@@ -1,12 +1,17 @@
+import javax.lang.model.util.ElementScanner6;
+
 public class CaixaEletronica {
     public static void main(String[] args) {
         double saldo = 25.0;
-        double valorSolicitado = 17.0;
+        double valorSolicitado = 22.0;
 
         if (valorSolicitado <= saldo) {
             saldo = saldo - valorSolicitado;
-
-            System.out.println(saldo);
+            System.out.println ("Novo Saldo: R$" + saldo);
         }
+        else    
+            System.out.println ("Saldo Insuficiente");
+         
+    
     }
 }
